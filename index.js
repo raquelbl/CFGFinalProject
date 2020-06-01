19ee54888fb1db0f9593c22e16546cab73ba62d2
raquelbl <raquelufsc@gmail.com>
2020-06-01 11:13:42 +0100

    Files upload

--- index.js
+++
@@ -1,7 +0,0 @@
-function validationform() {
-    var x = document.forms["getinfo"]["username"].value;
-    if (x == "") {
-        alert("You forgot to fill a Name");
-        return false;
-    }
-}
