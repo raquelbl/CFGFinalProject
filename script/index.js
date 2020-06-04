449228531c429aff3291632e25c205fc660ffb3f
raquelbl <raquelufsc@gmail.com>
2020-06-04 18:10:04 +0100

    updated

--- script/index.js
+++ script/index.js
@@ -1,7 +1,8 @@
-function validationform() {
-    var x = document.forms["getinfo"]["username"].value;
-    if (x == "") {
-        alert("You forgot to fill a Name");
-        return false;
-    }
+function validateform(){
+    var x = document.forms["guestinfo"]["User Name"].value;
+    if (x=="")
+{
+alert("Please fill your Name");
+return false;
+}
 }
